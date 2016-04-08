ef2aa4cbec8cb00c47c833df3779131174779620
Harsh Jain <hjain@xebia.com>
2016-04-08 20:59:45 +0530

    many to many done

--- src/main/java/com/xebia/jetter/model/Passenger.java
+++ src/main/java/com/xebia/jetter/model/Passenger.java
@@ -1,13 +1,10 @@
 package com.xebia.jetter.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.Set;
-
 
 /**
  * Created by gurinder on 8/4/16.
@@ -15,30 +12,26 @@ import java.util.Set;
 
 @Getter
 @Setter
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
-@Table(name = "flights")
-public class Flight {
+@Table(name = "passenger")
+public class Passenger {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
 
     @Column
-    private String flightName;
+    private String name;
 
     @Column
-    private boolean flyingStatus;
+    private String address;
 
     @Column
-    private String source;
-
-    @Column String destination;
+    private int age;
 
     @ManyToMany(cascade = CascadeType.ALL)
     @JoinTable(name = "flight_passenger", joinColumns = @JoinColumn(name =  "passenger_id", referencedColumnName = "id"),
-            inverseJoinColumns = @JoinColumn(name = "flight_id", referencedColumnName = "id"))
-    private Set<Passenger> passengers;
+    inverseJoinColumns = @JoinColumn(name = "flight_id", referencedColumnName = "id"))
+    private Set<Flight> flights;
 
 }
