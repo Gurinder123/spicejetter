2a8875120e9e719cd1d3eba5666b620fe6e49b1c
Gurinder singh <gurindersingh@xebia.com>
2016-04-13 12:50:16 +0530

    adding parameterized test with few modifications

--- src/main/java/com/xebia/jetter/service/LoginService.java
+++ src/main/java/com/xebia/jetter/service/LoginService.java
@@ -1,31 +1,10 @@
 package com.xebia.jetter.service;
 
-import com.xebia.jetter.dao.PassengerRepository;
 import com.xebia.jetter.model.LoginUser;
-import com.xebia.jetter.model.Passenger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 /**
- * Created with IntelliJ IDEA.
- * User: HJAIN
- * Date: 4/9/16
- * Time: 2:13 AM
- * To change this template use File | Settings | File Templates.
+ * Created by gurinder on 13/4/16.
  */
-@Service
-public class LoginService {
-
-    @Autowired
-    private PassengerRepository passengerRepository;
-
-    public boolean verifyLoginRequest(LoginUser loginUser) {
-        boolean flag = false;
-        Passenger user = passengerRepository.findByUsername(loginUser.getUsername());
-        if (user.getPassword().equals(loginUser.getPassword()) && user.getUsername().equals(loginUser.getUsername())) {
-            flag = true;
-        }
-
-        return flag;
-    }
+public interface LoginService {
+    boolean verifyLoginRequest(LoginUser loginUser);
 }
--- src/test/java/com/xebia/jetter/service/SpiceLoginTest.java
+++ src/test/java/com/xebia/jetter/service/SpiceLoginTest.java
@@ -5,9 +5,14 @@ import com.xebia.jetter.model.LoginUser;
 import com.xebia.jetter.model.Passenger;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
@@ -15,31 +20,47 @@ import static org.mockito.Mockito.when;
 /**
  * Created by gurinder on 12/4/16.
  */
-public class LoginServiceTest {
+@RunWith(Parameterized.class)
+public class SpiceLoginTest {
 
     @InjectMocks
-    LoginService loginService = new LoginService();
+    LoginService loginService = new SpiceLogin();
 
     @Mock
     PassengerRepository repository;
+
+    private LoginUser loginUser;
+    private boolean result;
 
     @Before
     public void init() {
         MockitoAnnotations.initMocks(this);
     }
 
+    public SpiceLoginTest(LoginUser loginUser, boolean result) {
+        this.loginUser = loginUser;
+        this.result = result;
+    }
+
+    @Parameterized.Parameters
+    public static Collection primeNumbers() {
+        return Arrays.asList(new Object[][]{
+                {new LoginUser("gsingh", "password"), true},
+                {new LoginUser("gsingh1", "password"), false}}
+        );
+    }
+
     @Test
-    public void testVerifyLoginRequest() {
+    public void testShouldVerifyLoginRequest() {
         LoginUser user = new LoginUser();
         user.setPassword("singh");
         user.setUsername("gsingh");
         Passenger passenger = new Passenger();
         passenger.setPassword("singh");
         passenger.setUsername("gsingh");
-
         when(repository.findByUsername("gsingh")).thenReturn(passenger);
-        boolean result = loginService.verifyLoginRequest(user);
-        assertEquals(true, result);
+        boolean result = loginService.verifyLoginRequest(loginUser);
+        assertEquals(result, result);
     }
 
 }
